73dc0f2a4995a88776250668a05324ae75397fe6
Suhani Makati <suhanimakati07@gmail.com>
2026-03-23 17:11:59 +0530

    Add files via upload

---
+++ Assignment 4.py
@@ -0,0 +1,40 @@
+#create dictionary 
+dictionary = {'name': 'Riya', 'age':18, 'city':'pune'}
+print(dictionary)
+#accessing dictionary elements
+print("accessing name from dictionary :", dictionary['name'])
+#adding an element to dictionary 
+dictionary['country'] = 'India'
+print("dictionary after adding country :", dictionary)
+#removing an element from dictionary 
+dictionary.pop('age')
+print("dictionary after removing age :", dictionary)
+#updating an element in dictionary
+dictionary['age'] = 19 
+print("dictionary after updating age :", dictionary)
+#keys of dictionary
+keys = dictionary.keys()
+print("keys of dictionary :", keys)
+#removing specific key
+dictionary.pop('city')
+print("dictionary after removing city :", dictionary)
+#create dictionary 
+dictionary = {'name': 'Riya', 'age':18, 'city':'pune'}
+print(dictionary)
+#accessing dictionary elements
+print("accessing name from dictionary :", dictionary['name'])
+#adding an element to dictionary 
+dictionary['country'] = 'India'
+print("dictionary after adding country :", dictionary)
+#removing an element from dictionary 
+dictionary.pop('age')
+print("dictionary after removing age :", dictionary)
+#updating an element in dictionary
+dictionary['age'] = 19 
+print("dictionary after updating age :", dictionary)
+#keys of dictionary
+keys = dictionary.keys()
+print("keys of dictionary :", keys)
+#removing specific key
+dictionary.pop('city')
+print("dictionary after removing city :", dictionary)
